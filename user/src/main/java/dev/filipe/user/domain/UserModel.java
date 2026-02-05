b01c37749cf28c1e1b46ec17f52b9a86061c9855
Filipe Lima <limafilipe.coding@gmail.com>
2026-02-04 21:55:16 -0300

    feat: inital user

---
+++ user/src/main/java/dev/filipe/user/domain/UserModel.java
@@ -0,0 +1,24 @@
+package dev.filipe.user.domain;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class UserModel {
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    private String name;
+    private String email;
+
+}
